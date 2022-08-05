297313df1653c65adbe43ac4c9484421756bcd1a
Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
2022-08-05 15:24:35 +0200

    Modify scripts and CI to support MacOS.
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>

--- extension/configure.sh
+++ extension/configure.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
 
-Z3_VERSION=4.8.17
+Z3_VERSION=4.10.2
 KIND2_VERSION=1.6.0
 SERVER_VERSION=0.1.0
 
+case "$1" in
+  macos*)
+    OSTYPE=darwin
+    ;;
+  linux*)
+    OSTYPE=linux
+    ;;
+  *)
+    ;;
+esac
+
+case "$OSTYPE" in
+  darwin*)
+    Z3_OS_VERSION=osx-10.16
+    KIND2_OS_VERSION=macos-10.15
+    ;;
+  linux*)
+    Z3_OS_VERSION=glibc-2.31
+    KIND2_OS_VERSION=linux
+    ;;
+  *)
+    echo "unknown: $OSTYPE"
+    ;;
+esac
+
 # Remove old configurations
-rm -r linux kind2-language-server
-rm z3-$Z3_VERSION-x64-glibc-2.31.zip*
-rm -r z3-$Z3_VERSION-x64-glibc-2.31
-rm kind2-v$KIND2_VERSION-linux-x86_64.tar.gz*
+rm -r z3 kind2 kind2-language-server
+rm z3-$Z3_VERSION-x64-$Z3_OS_VERSION.zip*
+rm -r z3-$Z3_VERSION-x64-$Z3_OS_VERSION
+rm kind2-v$KIND2_VERSION-$KIND2_OS_VERSION-x86_64.tar.gz*
 rm kind2-language-server.zip*
 
 # Install Z3
-mkdir linux
-wget https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/z3-$Z3_VERSION-x64-glibc-2.31.zip
-unzip -o z3-$Z3_VERSION-x64-glibc-2.31.zip
-rm z3-$Z3_VERSION-x64-glibc-2.31.zip
-cp z3-$Z3_VERSION-x64-glibc-2.31/bin/z3 linux
-rm -r z3-$Z3_VERSION-x64-glibc-2.31
+wget https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/z3-$Z3_VERSION-x64-$Z3_OS_VERSION.zip
+unzip -o z3-$Z3_VERSION-x64-$Z3_OS_VERSION.zip
+rm z3-$Z3_VERSION-x64-$Z3_OS_VERSION.zip
+cp z3-$Z3_VERSION-x64-$Z3_OS_VERSION/bin/z3 .
+rm -r z3-$Z3_VERSION-x64-$Z3_OS_VERSION
 
 # Install Kind 2
-wget https://github.com/kind2-mc/kind2/releases/download/v$KIND2_VERSION/kind2-v$KIND2_VERSION-linux-x86_64.tar.gz
-tar -xf kind2-v$KIND2_VERSION-linux-x86_64.tar.gz
-mv kind2 linux/kind2
-rm kind2-v$KIND2_VERSION-linux-x86_64.tar.gz
+wget https://github.com/kind2-mc/kind2/releases/download/v$KIND2_VERSION/kind2-v$KIND2_VERSION-$KIND2_OS_VERSION-x86_64.tar.gz
+tar -xf kind2-v$KIND2_VERSION-$KIND2_OS_VERSION-x86_64.tar.gz
+rm kind2-v$KIND2_VERSION-$KIND2_OS_VERSION-x86_64.tar.gz
 
 # Install language server for Kind 2
 wget https://github.com/kind2-mc/kind2-language-server/releases/download/$SERVER_VERSION/kind2-language-server.zip
