b45a38fa1c6a9e026d3afaa548dacac52efbb1aa
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2023-03-14 18:19:48 -0500

    Add option to compile static z3 binary

--- configure.sh
+++ configure.sh
@@ -65,16 +65,32 @@ KIND2_TAR_NAME=kind2-v$KIND2_VERSION-$KIND2_OS_VERSION
 # Remove old configurations
 rm -rf z3 kind2 kind2-language-server
 rm -f $Z3_ZIP_NAME.zip
-rm -rf $Z3_ZIP_NAME
+rm -rf $Z3_ZIP_NAME z3-z3-$Z3_VERSION
 rm -f $KIND2_TAR_NAME.tar.gz
 rm -f kind2-language-server.zip
 
 # Install Z3
-wget https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/$Z3_ZIP_NAME.zip
-unzip -o $Z3_ZIP_NAME.zip
-rm $Z3_ZIP_NAME.zip
-cp $Z3_ZIP_NAME/bin/z3 .
-rm -r $Z3_ZIP_NAME
+case "$2" in
+  z3-static)
+    wget https://github.com/Z3Prover/z3/archive/refs/tags/z3-$Z3_VERSION.tar.gz
+    tar xvf z3-$Z3_VERSION.tar.gz
+    rm z3-$Z3_VERSION.tar.gz
+    pushd z3-z3-$Z3_VERSION/
+    python3 scripts/mk_make.py --staticbin
+    pushd build/
+    make
+    popd
+    popd
+    cp z3-z3-$Z3_VERSION/build/z3 .
+    rm -r z3-z3-$Z3_VERSION/
+    ;;
+  *)
+    wget https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/$Z3_ZIP_NAME.zip
+    unzip -o $Z3_ZIP_NAME.zip
+    rm $Z3_ZIP_NAME.zip
+    cp $Z3_ZIP_NAME/bin/z3 .
+    rm -r $Z3_ZIP_NAME;;
+esac
 
 # Install Kind 2
 wget https://github.com/kind2-mc/kind2/releases/download/v$KIND2_VERSION/$KIND2_TAR_NAME.tar.gz
